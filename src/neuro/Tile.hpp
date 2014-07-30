51a0d0c456378d00ee0cecee583af8da9d72cec3
timorl <tymorl@gmail.com>
2014-07-30 11:10:02 +0200

    Added documentation, generate it using make doc

--- src/neuro/Tile.hpp
+++ src/neuro/Tile.hpp
@@ -6,6 +6,9 @@ namespace neuro {
 using Player=int;
 using Orientation=int;
 
+	/**
+		* @brief All the possible types of tiles.
+		*/
 	enum class TileType {
 		INSTANT_ACTION,
 		HQ,
@@ -14,44 +17,99 @@ using Orientation=int;
 		FOUNDATION
 	};
 
+	/**
+		* @brief A tile to be created in an army and later played.
+		* @todo Lists of abilities:
+		*  -playing
+		*  -on board
+		*  -permanent edge x6
+		*  -battle edge x6	
+		*/
 	class Tile : public ui::Observable<Tile> {
 		public:
-			Tile(TileType type, int health = 0, int initiative = -1) : type(type), health(health), initiative(initiative), orientation(0) {}
+			/**
+				* @brief Construct a tile with a specific type, health and initiative.
+				* @param[in] type Type of the tile.
+				* @param[in] health Health of the tile. Defaults to 0.
+				* @param[in] initiative Initiative of the tile. Defaults to 0.
+				*/
+			Tile(TileType type, int health = 0, int initiative = 0) : type(type), health(health), initiative(initiative), orientation(0) {}
 
+			/**
+				* @brief Returns the type of the tile.
+				*/
 			TileType getType() const { return type; }
 
+			/**
+				* @brief Returns the owner of the tile.
+				*/
 			Player getOwner() const { return owner; }
 
+			/**
+				* @brief Returns the current controller of the tile.
+				*/
 			Player getController() const { return controller; }
 
+			/**
+				* @brief Returns the current orientation of the tile.
+				*/
 			Orientation getOrientation() const { return orientation; }
 
+			/**
+				* @brief Returns the current health of the tile.
+				*/
 			int getHealth() const { return health; }
 
+			/**
+				* @brief Returns the current initiative of the tile if positive, else 0.
+				*/
 			int getInitiative() const { return (initiative > 0) ? initiative : 0; }
 
+			/**
+				* @brief Tells whether the tile is considered a solid object.
+				*/
 			bool isSolid() const { return (type == TileType::INSTANT_ACTION) || (type == TileType::FOUNDATION); }
 
+			/**
+				* @brief Sets the owner and original controller of the tile.
+				* @param[in] player The player to become the owner.
+				*/
 			void setOwner(Player player) {
 				owner = player;
 				controller = player;
 			}
 
+			/**
+				* @brief Changes the orientation of the tile.
+				* @param[in] orient The new orientation of the tile.
+				*/
 			void rotate(Orientation orient) {
 				orientation = orient;
 				sigModified(*this);
 			}
 
+			/**
+				* @brief Changes the controller of the tile.
+				* @param[in] player The new controller of the tile.
+				*/
 			void changeController(Player player) {
 				controller = player;
 				sigModified(*this);
 			}
 
+			/**
+				* @brief Modifies the health of the tile.
+				* @param[in] mod The amount of health to be added.
+				*/
 			void modifyHealth(int mod) {
 				health += mod;
 				sigModified(*this);
 			}
 
+			/**
+				* @brief Modifies the initiative of the tile.
+				* @param[in] mod The amount of initiative to be added.
+				*/
 			void modifyInitiative(int mod) {
 				initiative += mod;
 				sigModified(*this);
@@ -63,12 +121,6 @@ using Orientation=int;
 			Orientation orientation; // An integer between 0 and 5.
 			int health;
 			int initiative;
-			/* TODO -- Lists of abilities:
-				*  -playing
-				*  -on board
-				*  -permanent edge x6
-				*  -battle edge x6
-				*/
 	};
 
 }
--- src/ui/Observable.hpp
+++ src/ui/Observable.hpp
@@ -7,10 +7,20 @@
 namespace ui 
 {
 
+	/**
+		* @brief Inherit this if you want your class to be observable by UI elements.
+		*/
 	template <typename Derived>
 		class Observable 
 		{
 			public:
+				/**
+					* @brief A function being ran every time the inheriting object is modified.
+					* 
+					* This function should be provided by the observing object and will be called
+					* every time the inheriting object is modified in any way.
+					* The call will usually pass a reference to the modified object as an argument.
+					*/
 				mutable boost::signals2::signal<void (Derived const &)> sigModified;
 		};
 
