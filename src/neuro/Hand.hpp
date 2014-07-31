81ea9a7a94fe88169de04df73d3804fe8c43c6d5
timorl <tymorl@gmail.com>
2014-07-31 19:07:39 +0200

    Added Hand

---
+++ src/neuro/Hand.hpp
@@ -0,0 +1,45 @@
+#ifndef NEURO_HAND_HPP
+#define NEURO_HAND_HPP
+
+#include<list>
+#include"ui/Observable.hpp"
+#include"neuro/Tile.hpp"
+
+namespace neuro {
+
+	using TileP = std::shared_ptr< Tile >;
+
+	/**
+		* @brief A container for tiles being held in the hand of the player.
+		*/
+	class Hand : public ui::Observable<Hand> {
+		public:
+			/**
+				* @brief Construct an empty hand.
+				*/
+			Hand() {}
+
+			/**
+				* @brief Returns a std::list of pointers to tiles contained in the hand.
+				*/
+			std::list< TileP > const & getContents() const {	return tiles;	}
+
+			/**
+				* @brief Adds a tile to the hand.
+				* @param[in] tile The tile to add.
+				*/
+			void insertTile(TileP tile) { tiles.push_back(tile); sigModified(*this); }
+
+			/**
+				* @brief Removes the specified tile from the hand.
+				* @param[in] tilePos The position of the tile to remove.
+				*/
+			void removeTile(TileP tile) { tiles.remove(tile); sigModified(*this); }
+
+		private:
+			std::list< TileP > tiles;
+	};
+
+}
+
+#endif
