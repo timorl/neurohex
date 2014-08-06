5879cd092772bb9d378b01b8c672c0d390323ba4
timorl <tymorl@gmail.com>
2014-08-06 09:31:08 +0200

    Made the expected data directories more sane.

--- src/viewmodel/Application.cpp
+++ src/viewmodel/Application.cpp
@@ -3,10 +3,11 @@
 namespace viewmodel {
 
 	void Application::start() {
+		std::string programDirectory("/neurohex");
 		std::string globalDataDirectories( std::getenv("XDG_DATA_DIRS") );
-		std::string globalDataDirectory = globalDataDirectories.substr( 0, globalDataDirectories.find_first_of(':') );
+		std::string globalDataDirectory = globalDataDirectories.substr( 0, globalDataDirectories.find_first_of(':') ) + programDirectory;
 		std::string localDataDirectories( std::getenv("XDG_DATA_HOME") );
-		std::string localDataDirectory = localDataDirectories.substr( 0, localDataDirectories.find_first_of(':') );
+		std::string localDataDirectory = localDataDirectories.substr( 0, localDataDirectories.find_first_of(':') ) + programDirectory;
 		std::string boardSubdirectory("/boards");
 		boardLoader = BoardLoaderP( new BoardLoader() );
 		boardLoader->loadBoards(globalDataDirectory + boardSubdirectory);
